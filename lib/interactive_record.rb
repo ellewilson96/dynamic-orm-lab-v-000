7acadedb282691108003924e9628276d9cb4d42f
Elle Wilson <eleanorwilsonn@gmail.com>
2018-04-19 23:12:54 +0000

    automatically backed up by learn

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -5,8 +5,6 @@ class InteractiveRecord
   require_relative "../config/environment.rb"
 require 'active_support/inflector'
 
-class InteractiveRecord
-
   def self.table_name
     self.to_s.downcase.pluralize
   end
